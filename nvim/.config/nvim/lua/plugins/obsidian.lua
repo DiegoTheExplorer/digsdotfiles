23c540239dc28ffb86b40b605e40360e33d7f34f
Diego Villamil <dmvillamil@up.edu.ph>
2025-08-08 20:39:08 +0800

    Updated obsidian daily notes config and added keymaps for common commands

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -42,6 +42,7 @@ return {
       alias_format = '%B %-d, %Y',
       default_tags = { 'daily-journal' },
       template = 'journal-entry.md',
+      workdays_only = false,
     },
     completion = {
       nvim_cmp = false,
@@ -53,8 +54,20 @@ return {
   },
 
   -- Command keymaps
-  vim.keymap.set('n', '<leader>o', '<cmd>Obsidian<cr>', { desc = 'Open obsidian.nvim menu' }),
+  vim.keymap.set('n', '<leader>om', '<cmd>Obsidian<cr>', { desc = 'Open obsidian.nvim menu' }),
+
+  -- Creating new notes
   vim.keymap.set('n', '<leader>onn', '<cmd>Obsidian new<cr>', { desc = 'Create new blank obsidian note' }),
   vim.keymap.set('n', '<leader>ont', '<cmd>Obsidian new_from_template<cr>', { desc = 'Create new obsidian note from template' }),
-  vim.keymap.set('n', '<leader>ot', '<cmd>Obsidian today<cr>', { desc = 'Open or create the daily journal/note' }),
+  vim.keymap.set('n', '<leader>oit', '<cmd>Obsidian template<cr>', { desc = 'Choose template to insert into current note' }),
+
+  -- Daily notes/journal shortcuts
+  vim.keymap.set('n', '<leader>otd', '<cmd>Obsidian today<cr>', { desc = 'Open or create the @today daily journal/note' }),
+  vim.keymap.set('n', '<leader>oyd', '<cmd>Obsidian yesterday<cr>', { desc = 'Open or create the @yesterday daily journal/note' }),
+  vim.keymap.set('n', '<leader>otm', '<cmd>Obsidian tomorrow<cr>', { desc = 'Open or create the @tomorrow daily journal/note' }),
+
+  -- Searching through the notes
+  vim.keymap.set('n', '<leader>osn', '<cmd>Obsidian search<cr>', { desc = 'Grep through all notes in the vault' }),
+  vim.keymap.set('n', '<leader>ost', '<cmd>Obsidian tags<cr>', { desc = 'Pick a tag and search through notes with the chosen tag' }),
+  vim.keymap.set('n', '<leader>otc', '<cmd>Obsidian toc<cr>', { desc = 'Load the table of contents for the current not into a picker list' }),
 }
