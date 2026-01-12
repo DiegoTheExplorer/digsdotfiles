911733494da29688ef227709fca3b8d59496ccd8
Diego Villamil <dmvillamil@up.edu.ph>
2026-01-12 19:38:28 +0800

    Configured treesitter-textobjects for the treesitter main branch

--- nvim/.config/nvim/lua/plugins/treesitter-modules.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-modules.lua
@@ -3,64 +3,41 @@ return {
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
   ---@module 'treesitter-modules'
   ---@type ts.mod.UserConfig
-  opts = {
-    ensure_installed = {
-      'bash',
-      'c',
-      'diff',
-      'lua',
-      'luadoc',
-      'markdown',
-      'markdown_inline',
-      'query',
-      'vim',
-      'vimdoc',
-      'java',
-      'regex',
-      'sql',
-      'html',
-      'css',
-      'javascript',
-      'typescript',
-      'svelte',
-    },
-    ignore_install = { 'latex' },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
+  config = function()
+    require('treesitter-modules').setup {
 
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<Leader>ss',
-        node_incremental = '<Leader>si',
-        scope_incremental = '<Leader>sc',
-        node_decremental = '<Leader>sd',
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'java',
+        'regex',
+        'sql',
+        'html',
+        'css',
+        'javascript',
+        'typescript',
+        'svelte',
       },
-    },
+      ignore_install = { 'latex' },
+      auto_install = true,
 
-    textobjects = {
-      enable = true,
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = { query = '@class.inner', desc = 'Select inner part of a class region' },
-        ['as'] = { query = '@local.scope', query_group = 'locals', desc = 'Select language scope' },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<Leader>ss',
+          node_incremental = '<Leader>si',
+          scope_incremental = '<Leader>sc',
+          node_decremental = '<Leader>sd',
+        },
       },
-
-      selection_modes = {
-        ['@parameter.outer'] = 'v', -- charwise
-        ['@function.outer'] = 'V', -- linewise
-        ['@class.outer'] = '<c-v>', -- blockwise
-      },
-      include_surrounding_whitespace = true,
-    },
-  },
+    }
+  end,
 }
--- nvim/.config/nvim/lua/plugins/treesitter-textobjects.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-textobjects.lua
@@ -1,4 +1,36 @@
 return {
   'nvim-treesitter/nvim-treesitter-textobjects',
   branch = 'main',
+  config = function()
+    require('nvim-treesitter-textobjects').setup {
+      enable = true,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+
+      selection_modes = {
+        ['@parameter.outer'] = 'v', -- charwise
+        ['@function.outer'] = 'V', -- linewise
+        ['@class.outer'] = '<c-v>', -- blockwise
+      },
+      include_surrounding_whitespace = true,
+    }
+
+    local select = require('nvim-treesitter-textobjects.select').select_textobject
+
+    vim.keymap.set({ 'x', 'o' }, 'af', function()
+      select('@function.outer', 'textobjects')
+    end, { desc = 'Select around function' })
+    vim.keymap.set({ 'x', 'o' }, 'if', function()
+      select('@function.inner', 'textobjects')
+    end, { desc = 'Select inside function' })
+    vim.keymap.set({ 'x', 'o' }, 'ac', function()
+      select('@class.outer', 'textobjects')
+    end, { desc = 'Select around class' })
+    vim.keymap.set({ 'x', 'o' }, 'ic', function()
+      select('@class.inner', 'textobjects')
+    end, { desc = 'Select inside class' })
+    vim.keymap.set({ 'x', 'o' }, 'as', function()
+      select('@local.scope', 'locals')
+    end, { desc = 'Select around local scope' })
+  end,
 }
