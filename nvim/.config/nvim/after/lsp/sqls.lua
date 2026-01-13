7650721d1833bd280e6b7329d6a5f7e046087711
Diego Villamil <dmvillamil@up.edu.ph>
2026-01-13 16:52:44 +0800

    Added missing opt from blink.cmp config and disabled formatting for sqls

--- nvim/.config/nvim/after/lsp/sqls.lua
+++ nvim/.config/nvim/after/lsp/sqls.lua
@@ -4,4 +4,8 @@ return {
   filetypes = { 'sql', 'mysql' },
   root_markers = { 'config.yml' },
   settings = {},
+  on_attach = function(client)
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+  end,
 }
